e52f3b3811baa25155a2b491fb9e1247bf1c32f8
shouzhen <s25gu@uwaterloo.ca>
2015-03-21 00:29:58 -0400

    basic working function

--- AlgoTradingComp/client.m.cpp
+++ AlgoTradingComp/client.m.cpp
@@ -17,89 +17,55 @@ using namespace std;
 using namespace galik;
 using namespace galik::net;
 
-struct Transaction{
-    string ticker;
-    double price;
-    int shares;
-    Transaction(string ticker, double price, int shares){
-        this->ticker = ticker;
-        this->price = price;
-        this->shares = shares;
-    }
-};
-
 socketstream ss;
-vector<Transaction> buys;
-vector<Transaction> sells;
-
-std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
-	size_t start = 0, end = 0;
-	while ((end = s.find(delim, start)) != string::npos) {
-		elems.push_back(s.substr(start, end - start));
-		start = end + 1;
-	}
-	elems.push_back(s.substr(start));
-	return elems;
-}
 
 double myCash(){
-    ss << "MY_CASH";
-    while (ss.good() && !ss.eof()){
-        string line;
-        getline(ss, line);
-        string token = line.substr(0, line.find(' '));
-        line.erase(0, line.find(' ')+1);
-        if (token == "BUY" || token == "SELL"){
-            string ticker = line.substr(0, line.find(' '));
-            line.erase(0, line.find(' ')+1);
-            double price = atof(line.substr(0, line.find(' ')).c_str());
-            line.erase(0, line.find(' ')+1);
-            int shares = atoi(line.substr(0, line.find(' ')).c_str());
-            line.erase(0, line.find(' ')+1);
-            if (token == "BUY")
-                buys.push_back(Transaction(ticker, price, shares));
-            if (token == "SELL")
-                buys.push_back(Transaction(ticker, price, shares));
+    ss << "MY_CASH " << endl;
+    if (ss.good() && !ss.eof()) {
+        string token;
+        ss >> token;
+        if (token == "MY_CASH_OUT"){
+            double cash;
+            ss >> cash;
+            return cash;
         }
-        else if (token == "MY_CASH_OUT")
-            return atof(line.c_str());
     }
 }
 
-
 int main() {
-    //setup
+
     string name = "lisgarppls";
     string password = "doeobdi";
     string host = "codebb.cloudapp.net";
     int port = 17429;
+    string command = "";
+
     ss.open(host, port);
-	ss << name << " " << password << "\n" << "SUBSCRIBE" << endl;
+	ss << name << " " << password << endl;
 
-	for (int i=0; i<100; i++){
-        /*write actual code here*/
-        cout << myCash();
-
-        while (ss.good() && !ss.eof()){
-            string line;
-            getline(ss, line);
-            string token = line.substr(0, line.find(' '));
-            line.erase(0, line.find(' ')+1);
-            if (token == "BUY" || token == "SELL"){
-                string ticker = line.substr(0, line.find(' '));
-                line.erase(0, line.find(' ')+1);
-                double price = atof(line.substr(0, line.find(' ')).c_str());
-                line.erase(0, line.find(' ')+1);
-                int shares = atoi(line.substr(0, line.find(' ')).c_str());
-                line.erase(0, line.find(' ')+1);
-                if (token == "BUY")
-                    buys.push_back(Transaction(ticker, price, shares));
-                if (token == "SELL")
-                    buys.push_back(Transaction(ticker, price, shares));
-            }
-        }
-	}
-
-    ss << "\nCLOSE_CONNECTION" << endl;
-	return 0;
+	cout << myCash();
+    ss << "CLOSE_CONNECTION" << endl;
+    return 0;
+//    while(true){
+//        getline(cin,command);
+//        cout << "Command:" << command << endl;
+//
+//        if(command == "close"){
+//            ss << "CLOSE_CONNECTION" << endl;
+//            return 0;
+//        }
+//        else if (command == "mycash")
+//            cout << myCash();
+//        else{
+//            ss << command << " " << endl;
+//        }
+//
+//        cout << "lala: " << (ss.good() && !ss.eof()) << endl;
+//        if (ss.good() && !ss.eof()) {
+//            string line;
+//            getline(ss, line);
+//            cout << line << endl;
+//        }
+//        cout << "cycle done" << endl << endl;
+//    }
 }
